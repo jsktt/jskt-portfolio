a47cad0c25b121ddad49e69f74acee3ba9cf630e
Junsung Kim <uitomde@gmail.com>
2026-02-24 14:31:34 +0900

    #1 setting: 라이브러리 설치

---
+++ setting_script.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+echo "installing react-query..."
+
+npm install @tanstack/react-query
+
+echo "installed react-query"
+
+echo "installing react-router-dom..."
+
+npm install react-router-dom
+
+echo "installed react-router-dom!"
+
+echo "installing redux toolkit..."
+
+npm install @reduxjs/toolkit react-redux
+
+echo "installed redux toolkit!"
+
+echo "installing supabase"
+
+npm install @supabase/supabase-js
+
+echo "installed supabase!"
