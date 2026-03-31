0b5de436bc014a461e94b6b49bd88aa327bb2aa1
Junsung Kim <uitomde@gmail.com>
2026-03-31 10:23:16 +0900

    fixing header errors

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,14 +13,14 @@ const Header = () => {
         <Link to="/">JUNSUNG KIM</Link>{" "}
       </div>
 
-      {user ? <Link to="/projects/new"> NEW PROJECT </Link> : <p>{""}</p>}
+      {/* {user ? <Link to="/projects/new"> NEW PROJECT </Link> : <p>{""}</p>} */}
       {user ? <Link to="/writings/new"> NEW WRITING </Link> : <p>{""}</p>}
 
       <nav className={styles.right}>
         <Link to="/about" state={{ backgroundLocation: location }}>
           ABOUT
         </Link>
-        <Link to="/projects">PROJECTS</Link>
+        {/* <Link to="/projects">PROJECTS</Link> */}
         <Link to="/writings">WRITINGS</Link>
       </nav>
     </header>
