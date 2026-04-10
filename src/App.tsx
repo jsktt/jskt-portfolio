8360ee7bb789091e86491589d860c77768cd26b4
Junsung <uitomde@gmail.com>
2026-04-10 10:35:08 +0900

    fixed version error for vercel speed-insights

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import Home from "./pages/Home/Home";
 import BlogForm from "./pages/BlogForm/BlogForm";
 import BlogDetail from "./pages/BlogDetail/BlogDetail";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 
